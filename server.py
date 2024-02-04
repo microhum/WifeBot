66cde98893c75c62e9194dbfa540bfb115fdd497
microhum <Guntee12123@gmail.com>
2024-02-05 00:35:21 +0700

    Add Flask Server

---
+++ server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from waitress import serve
+
+app = Flask(__name__)
+
+@app.route("/")
+def returnHTML():
+    return "Hi, This is Wife-Bot Hosting with render.com service"
+
+if __name__ == "__main__":
+    # app.run(host="0.0.0.0", port=80)
+    serve(app, host='0.0.0.0', port=80)
